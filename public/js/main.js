99e95ecea8d0df3ec81131e8059c28178d000faa
fkysly <fkysly@gmail.com>
2014-12-02 14:26:22 +0800

    优化首页
    优化首页

--- public/js/main.js
+++ public/js/main.js
@@ -1,53 +1,56 @@
-//首页left-nav控制
-window.onscroll = function(){ 
-    var t = document.documentElement.scrollTop || document.body.scrollTop;  
-    var left_div = document.getElementById( "nav_left" ); 
-    if( t >= 50 ) { 
-        left_div.style.top="0";
-    } else { 
-          left_div.style.top="50px";
-    } 
-} 
+// 防知乎首页模块
+var foucs_module = (function(window, $) {
 
+    var tabcurrent, level, tablock, start;
 
-if(document.body.scrollWidth < 748){
-      document.getElementById('nav_left').style.display = 'none';
-    }
-//首页left-nav控制
-
-
-
-//首页仿知乎风格的js代码
-
-
-        var tabcurrent = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1,1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
-        function foucs(c, i, str) {
-            if (tabcurrent[i] == c) {
-                return;
-            }
-            $("#" + str + "_tab_" + tabcurrent[i]).removeClass("on");
-            $("#" + str + "_tab_" + tabcurrent[i]).removeClass("current");
-            $("#" + str + "_con_" + tabcurrent[i]).hide();
-            tabcurrent[i] = c;
-            $("#" + str + "_tab_" + tabcurrent[i]).addClass("on");
-            $("#" + str + "_tab_" + tabcurrent[i]).addClass("current");
-            $("#" + str + "_con_" + tabcurrent[i]).show();
+    var foucs = function foucs(c, i, str) {
+        if (tabcurrent[i] == c) {
+            return;
         }
-        var level = setInterval("autoplay()", 30000);
-        var tablock = 0;
-        var start = 1;
-        function autoplay() {
-            if (tablock) return;
-            foucs(start, '2', 'foucs');
-            start++;
-            if (start == 16)/*页卡有n项，这里的数字就是n+1*/
-                start = 1;
-        }
-        for (var l = 1; l <16; l++)/*页卡有n项，这里的数字就是n+1*/ {
+        $("#" + str + "_tab_" + tabcurrent[i]).removeClass("on");
+        $("#" + str + "_tab_" + tabcurrent[i]).removeClass("current");
+        $("#" + str + "_con_" + tabcurrent[i]).hide();
+        tabcurrent[i] = c;
+        $("#" + str + "_tab_" + tabcurrent[i]).addClass("on");
+        $("#" + str + "_tab_" + tabcurrent[i]).addClass("current");
+        $("#" + str + "_con_" + tabcurrent[i]).show();
+    };
+    
+    var autoplay = function autoplay() {
+        if (tablock) return;
+        foucs(start, '2', 'foucs');
+        start++;
+        if (start == 16)/*页卡有n项，这里的数字就是n+1*/
+            start = 1;
+    };
+
+    var init = function() {
+        $(".rep").each(function() {
+            var index = this.id.split('_')[2];
+            $(this).mouseover(function() {
+                foucs(index,2,'foucs');
+            });
+        });
+    };
+
+    var run = function() {
+        init();
+        tabcurrent = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+        level = setInterval(autoplay, 30000);
+        tablock = 0;
+        start = 1;
+        for (var l = 1; l < 16; l++)/*页卡有n项，这里的数字就是n+1*/ {
             $("#foucs_tab_" + l).mouseover(function () { tablock = 1; });
             $("#foucs_tab_" + l).mouseout(function () { tablock = 0; });
             $("#foucs_con_" + l).mouseover(function () { tablock = 1; });
             $("#foucs_con_" + l).mouseout(function () { tablock = 0; });
         }
+    };
 
-//首页仿知乎风格的js代码        
+    return {
+        run : run
+    };
+
+})(window, jQuery);
+
+foucs_module.run();
